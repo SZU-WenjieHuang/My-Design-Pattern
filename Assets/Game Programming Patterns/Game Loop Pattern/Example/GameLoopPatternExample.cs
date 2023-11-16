2dbe10a43b3e9be5f197d7bedb558772db777b4d
Patrick <Jankinhuang0302@163.com>
2023-11-16 11:28:12 +0800

    游戏循环的一个examples

--- Assets/Game Programming Patterns/Game Loop Pattern/Example/GameLoopPatternExample.cs
+++ Assets/Game Programming Patterns/Game Loop Pattern/Example/GameLoopPatternExample.cs
@@ -5,7 +5,29 @@
 using UnityEngine;
 using System.Collections;
 
+/*
+    这段代码实现了一个基本的游戏循环模式,主要内容包括:
 
+    定义了一个固定的逻辑更新间隔时间MS_PER_UPDATE,通常会设置为大约16毫秒左右,对应60FPS。
+
+    在循环中计算两帧之间的实际流逝时间elapsed。
+
+    通过lag变量累积流逝时间,当lag达到或超过MS_PER_UPDATE时,执行一次逻辑更新Update()。
+
+    每帧都执行渲染函数Render(),与逻辑更新脱钩。
+
+    ProcessInput()处理输入消息。
+
+    Update()执行游戏状态逻辑更新。
+
+    用while(true)实现一个无限循环。
+
+    这样可以使游戏内部的逻辑更新定时稳定执行,与图形渲染的帧率变化解耦合,防止帧率波动导致游戏速度异常。
+
+    lag变量起到调节作用,在帧率不均时动态调整逻辑更新次数,来维持内部时间流逝的稳定。
+
+    整体上实现了一个基本的游戏循环核心逻辑,适合不同的游戏开发需要。
+ */
 
 namespace GameLoopPatternExample
 {
@@ -48,7 +70,7 @@ namespace GameLoopPatternExample
     {
 
         /// <summary>
-        /// 游戏更新的粒度
+        /// 游戏更新的粒度 大概60fps
         /// </summary>
        public const float MS_PER_UPDATE = 0.06F;
        
@@ -68,11 +90,13 @@ namespace GameLoopPatternExample
                 //当前时间
                 double current = Time.realtimeSinceStartup;
                 //消逝的时间
+
                 double elapsed = current - previous;
                 previous = current;
                 lag += elapsed;
                 ProcessInput();
 
+                //游戏逻辑与渲染解耦
                 while (lag >= MS_PER_UPDATE)
                 {
                     Update();
